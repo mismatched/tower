744a61f7804ebfe19a51516d49719640a8b26b12
Dariush Abbasi <dariushem@yahoo.com>
2019-07-02 23:33:38 +0430

    init doping

---
+++ doping.go
@@ -0,0 +1,50 @@
+// doping
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptrace"
+	"os"
+	"time"
+)
+
+func main() {
+	doping(os.Args[1])
+}
+
+func doping(url string) {
+	req, _ := http.NewRequest("GET", url, nil)
+
+	var start, connect, dns, tlsHandshake time.Time
+
+	trace := &httptrace.ClientTrace{
+		DNSStart: func(dsi httptrace.DNSStartInfo) { dns = time.Now() },
+		DNSDone: func(ddi httptrace.DNSDoneInfo) {
+			fmt.Printf("DNS Done: %v\n", time.Since(dns))
+		},
+
+		TLSHandshakeStart: func() { tlsHandshake = time.Now() },
+		TLSHandshakeDone: func(cs tls.ConnectionState, err error) {
+			fmt.Printf("TLS Handshake: %v\n", time.Since(tlsHandshake))
+		},
+
+		ConnectStart: func(network, addr string) { connect = time.Now() },
+		ConnectDone: func(network, addr string, err error) {
+			fmt.Printf("Connect time: %v\n", time.Since(connect))
+		},
+
+		GotFirstResponseByte: func() {
+			fmt.Printf("Time from start to first byte: %v\n", time.Since(start))
+		},
+	}
+
+	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
+	start = time.Now()
+	if _, err := http.DefaultTransport.RoundTrip(req); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Total time: %v\n", time.Since(start))
+}
