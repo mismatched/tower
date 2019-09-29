b085bb8143d19d9e4d5b8682af3f4230c16178f9
Dariush Abbasi <dariushem@yahoo.com>
2019-09-29 22:49:01 +0330

    init testing with dns test

---
+++ lib/dns_test.go
@@ -0,0 +1,18 @@
+package libtower
+
+import (
+	"net"
+	"reflect"
+	"testing"
+)
+
+func TestDNSLookup(t *testing.T) {
+	addr, err := DNSLookup("google.com")
+	if err != nil {
+		t.Errorf("test failed %v", err)
+	}
+	var b *net.IPAddr
+	if reflect.TypeOf(addr) != reflect.TypeOf(b) {
+		t.Errorf("its not a valid ip address")
+	}
+}
