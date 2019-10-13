7ad11ed288533b05aa35d5a5253dbaea2c72c6fa
Dariush Abbasi <dariushem@yahoo.com>
2019-10-13 18:55:54 +0330

    add http status calculator basic function

--- lib/httpstatus.go
+++ lib/httpstatus.go
@@ -1,6 +1,9 @@
 package libtower
 
-import "time"
+import (
+	"net/http"
+	"time"
+)
 
 // HTTPStatusResult type
 type HTTPStatusResult struct {
@@ -29,5 +32,24 @@ type HTTPStatusResult struct {
 
 // HTTPStatus check
 func HTTPStatus(url, method string) (HTTPStatusResult, error) {
-	return HTTPStatusResult{}, nil
+	// setup client
+	hsr := HTTPStatusResult{}
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "http://example.com", nil)
+	if err != nil {
+		return hsr, err
+	}
+
+	req.Header.Add("If-None-Match", `W/"wyzzy"`)
+	resp, err := client.Do(req)
+	if err != nil {
+		return hsr, err
+	}
+
+	hsr.Start = time.Now()
+	hsr.StatusCode = resp.StatusCode
+	hsr.End = time.Now()
+	hsr.Duration = hsr.Start.Sub(hsr.End)
+
+	return hsr, nil
 }
