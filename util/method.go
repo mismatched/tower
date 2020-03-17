885e6c704b6dc37b984ec85bfe826fb8ca569b56
Dariush Abbasi <dariushem@yahoo.com>
2020-03-17 23:55:19 +0330

    add default http method

--- util/method.go
+++ util/method.go
@@ -2,14 +2,22 @@ package util
 
 import "strings"
 
+// DefaultHTTPMethod for not specified methods
+const DefaultHTTPMethod = "GET"
+
 // HTTPMethod returns the currect http method
+// Default method: GET
 func HTTPMethod(arg string) string {
 	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE"}
+
 	arg = strings.ToUpper(arg)
+
 	for _, m := range methods {
 		if m == arg {
 			return arg
 		}
 	}
-	return "GET"
+
+	// return default method
+	return DefaultHTTPMethod
 }
